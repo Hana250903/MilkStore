a612964686a9cd753dd9b863cbe955b8eb413cb3
Lê Trọng Nhân <127916666+Hana250903@users.noreply.github.com>
2024-05-30 11:23:21 +0700

    Upload file SQL

---
+++ MilkStore.sql
@@ -0,0 +1,286 @@
+CREATE DATABASE [MilkStore]
+
+USE [MilkStore]
+GO
+/****** Object:  Table [dbo].[Admin]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Admin](
+	[AdminId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Desciption] [nvarchar](10) NOT NULL,
+ CONSTRAINT [PK_Admin] PRIMARY KEY CLUSTERED 
+(
+	[AdminId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Brand]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Brand](
+	[BrandId] [int] IDENTITY(1,1) NOT NULL,
+	[BrandName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Brand] PRIMARY KEY CLUSTERED 
+(
+	[BrandId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Comment]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Comment](
+	[CommentId] [int] IDENTITY(1,1) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[DateCreate] [date] NOT NULL,
+	[Content] [nvarchar](500) NOT NULL,
+	[Picture] [nvarchar](100) NOT NULL,
+	[Rate] [float] NOT NULL,
+	[MilkId] [int] NOT NULL,
+ CONSTRAINT [PK_Comment] PRIMARY KEY CLUSTERED 
+(
+	[CommentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Member]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Member](
+	[MemberId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Desciption] [nvarchar](10) NOT NULL,
+ CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED 
+(
+	[MemberId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Milk]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Milk](
+	[MilkId] [int] IDENTITY(1,1) NOT NULL,
+	[MilkName] [nvarchar](50) NOT NULL,
+	[BrandId] [int] NOT NULL,
+	[Capacity] [int] NOT NULL,
+	[MilkTypeId] [int] NOT NULL,
+	[AppropriateAge] [nvarchar](50) NOT NULL,
+	[StorageInstructions] [nvarchar](50) NOT NULL,
+	[Price] [float] NOT NULL,
+	[Discount] [float] NOT NULL,
+ CONSTRAINT [PK_Milk] PRIMARY KEY CLUSTERED 
+(
+	[MilkId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[MilkPicture]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MilkPicture](
+	[MilkPictureId] [int] IDENTITY(1,1) NOT NULL,
+	[MilkId] [int] NOT NULL,
+	[Picture] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_MilkPicture] PRIMARY KEY CLUSTERED 
+(
+	[MilkPictureId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[MilkType]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MilkType](
+	[MilkTypeId] [nvarchar](50) NOT NULL,
+	[TypeName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_MilkType] PRIMARY KEY CLUSTERED 
+(
+	[MilkTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Order]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Order](
+	[OrderId] [int] IDENTITY(1,1) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[VoucherId] [int] NOT NULL,
+	[DateCreate] [date] NOT NULL,
+	[Amount] [float] NOT NULL,
+	[OrderStatus] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_OrderDetail] PRIMARY KEY CLUSTERED 
+(
+	[OrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderDetail]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetail](
+	[OrderDetailId] [int] IDENTITY(1,1) NOT NULL,
+	[OrderId] [int] NOT NULL,
+	[MilkId] [int] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[Total] [float] NOT NULL,
+ CONSTRAINT [PK_Cart] PRIMARY KEY CLUSTERED 
+(
+	[OrderDetailId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Role]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Role](
+	[RoleId] [int] IDENTITY(1,1) NOT NULL,
+	[RoleName] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Staff]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Staff](
+	[StaffId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[Desciption] [nvarchar](10) NOT NULL,
+ CONSTRAINT [PK_Staff] PRIMARY KEY CLUSTERED 
+(
+	[StaffId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](20) NOT NULL,
+	[Phone] [nvarchar](10) NOT NULL,
+	[DateOfBirth] [date] NOT NULL,
+	[Gender] [nvarchar](10) NOT NULL,
+	[Address] [nvarchar](100) NOT NULL,
+	[RoleId] [int] NOT NULL,
+	[ProfilePicture] [nvarchar](100) NOT NULL,
+	[DateCreate] [date] NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Voucher]    Script Date: 30/05/2024 11:13:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Voucher](
+	[VoucherId] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](50) NOT NULL,
+	[StartDate] [date] NOT NULL,
+	[EndDate] [date] NOT NULL,
+	[Discount] [float] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[Status] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_Voucher] PRIMARY KEY CLUSTERED 
+(
+	[VoucherId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Admin]  WITH CHECK ADD  CONSTRAINT [FK_Admin_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[Admin] CHECK CONSTRAINT [FK_Admin_User]
+GO
+ALTER TABLE [dbo].[Comment]  WITH CHECK ADD  CONSTRAINT [FK_Comment_Member] FOREIGN KEY([MemberId])
+REFERENCES [dbo].[Member] ([MemberId])
+GO
+ALTER TABLE [dbo].[Comment] CHECK CONSTRAINT [FK_Comment_Member]
+GO
+ALTER TABLE [dbo].[Comment]  WITH CHECK ADD  CONSTRAINT [FK_Comment_Milk] FOREIGN KEY([MilkId])
+REFERENCES [dbo].[Milk] ([MilkId])
+GO
+ALTER TABLE [dbo].[Comment] CHECK CONSTRAINT [FK_Comment_Milk]
+GO
+ALTER TABLE [dbo].[Member]  WITH CHECK ADD  CONSTRAINT [FK_Member_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[Member] CHECK CONSTRAINT [FK_Member_User]
+GO
+ALTER TABLE [dbo].[Milk]  WITH CHECK ADD  CONSTRAINT [FK_Milk_Brand] FOREIGN KEY([BrandId])
+REFERENCES [dbo].[Brand] ([BrandId])
+GO
+ALTER TABLE [dbo].[Milk] CHECK CONSTRAINT [FK_Milk_Brand]
+GO
+ALTER TABLE [dbo].[Milk]  WITH CHECK ADD  CONSTRAINT [FK_Milk_MilkType] FOREIGN KEY([AppropriateAge])
+REFERENCES [dbo].[MilkType] ([MilkTypeId])
+GO
+ALTER TABLE [dbo].[Milk] CHECK CONSTRAINT [FK_Milk_MilkType]
+GO
+ALTER TABLE [dbo].[MilkPicture]  WITH CHECK ADD  CONSTRAINT [FK_MilkPicture_Milk] FOREIGN KEY([MilkId])
+REFERENCES [dbo].[Milk] ([MilkId])
+GO
+ALTER TABLE [dbo].[MilkPicture] CHECK CONSTRAINT [FK_MilkPicture_Milk]
+GO
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Member] FOREIGN KEY([MemberId])
+REFERENCES [dbo].[Member] ([MemberId])
+GO
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_OrderDetail_Member]
+GO
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Voucher] FOREIGN KEY([VoucherId])
+REFERENCES [dbo].[Voucher] ([VoucherId])
+GO
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_OrderDetail_Voucher]
+GO
+ALTER TABLE [dbo].[OrderDetail]  WITH CHECK ADD  CONSTRAINT [FK_Cart_Milk] FOREIGN KEY([OrderId])
+REFERENCES [dbo].[Milk] ([MilkId])
+GO
+ALTER TABLE [dbo].[OrderDetail] CHECK CONSTRAINT [FK_Cart_Milk]
+GO
+ALTER TABLE [dbo].[OrderDetail]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Order] FOREIGN KEY([OrderId])
+REFERENCES [dbo].[Order] ([OrderId])
+GO
+ALTER TABLE [dbo].[OrderDetail] CHECK CONSTRAINT [FK_OrderDetail_Order]
+GO
+ALTER TABLE [dbo].[Staff]  WITH CHECK ADD  CONSTRAINT [FK_Staff_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+ALTER TABLE [dbo].[Staff] CHECK CONSTRAINT [FK_Staff_User]
+GO
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Role] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Role] ([RoleId])
+GO
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Role]
+GO
